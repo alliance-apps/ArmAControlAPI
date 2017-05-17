7eb7039c857e00c96e4a70ddd0e9c66c22027611
Tim Vogler <timvogler@me.com>
2017-05-17 18:14:27 +0200

    Test

--- app/Http/Controllers/VehicleController.php
+++ app/Http/Controllers/VehicleController.php
@@ -92,7 +92,7 @@ class VehicleController extends Controller
 
     public function deleteVehicle($vid)
     {
-        DB::table('vehicles')->where('id', $vid)->update(['alive' => 0])->save();
+        DB::table('vehicles')->where('id', $vid)->update(['alive' => 0]);
         $vehicle = DB::table('vehicles')->where('id', $vid)->first();
         $player = DB::table('players')->where('pid', $vehicle->pid)->first();
         $playerid = $player->uid;
