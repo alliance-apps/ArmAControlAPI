10ffb4022c83fec101ec18ebf0132e8f770775f9
Tim Vogler <timvogler@me.com>
2017-09-09 15:18:05 +0200

    Adding Shared API Decrypting + Config

---
+++ app/Http/Middleware/sharedApi.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Crypt;
+use League\Flysystem\Config;
+
+class sharedApi
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (!env('SHAREDAPI', false))
+        {
+            return $next($request);
+        }
+
+        $payload = $request->db;
+        $db = Crypt::decrypt($payload);
+        $db = json_decode($db);
+        config(['database.connections.mysql.host' => $db->host]);
+        config(['database.connections.mysql.database' => $db->database]);
+        config(['database.connections.mysql.username' => $db->username]);
+        config(['database.connections.mysql.password' => $db->password]);
+        config(['database.connections.mysql.port' => $db->port]);
+        return $next($request);
+    }
+}
